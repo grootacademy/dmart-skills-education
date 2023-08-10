eaecbf4ed5a2ee23cd8bcdbba018a60079fd305b
satnamkhowal <satnamkhowal@gmail.com>
2023-08-10 18:31:26 +0530

    bjhsdajd

--- includes/connection.php
+++ includes/connection.php
@@ -1,15 +1,15 @@
 <?php 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$my_db = 'groot_software';
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $my_db = 'groot_software';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$my_db);
+// // Create connection
+// $conn = new mysqli($servername, $username, $password,$my_db);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
 // echo "Connected successfully";
 ?>
