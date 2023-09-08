c4615cc42d9c3772ec626d0d09099b93ce22f4c3
satnamkhowal <satnamkhowal@gmail.com>
2023-09-08 23:36:29 +0530

    google tag manager added

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,24 @@
 <html lang="en">
 
 <head>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-N3TMZ6WF');
+    </script>
+    <!-- End Google Tag Manager -->
     <!-- meta tag -->
     <meta charset="utf-8">
 
@@ -83,7 +101,10 @@
 </head>
 
 <body class="defult-home">
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3TMZ6WF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!--Preloader area start here-->
     <div id="loader" class="loader green-color">
         <div class="loader-container">
